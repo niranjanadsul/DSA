f5868efb20c65eec805588925d50ace8b9046e90
niranjanadsul <niranjanadsul96@gmail.com>
2025-05-24 14:17:07 +0530

    Added Morgan Stanley questions for online test

---
+++ src/MorganStanleyTest/TestScore.java
@@ -0,0 +1,11 @@
+package MorganStanleyTest;
+
+public class TestScore {
+    String team;
+    int score;
+
+    public TestScore(String team, int score) {
+        this.team = team;
+        this.score = score;
+    }
+}
