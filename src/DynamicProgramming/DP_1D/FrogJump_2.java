edf02a68eecdc769331c11bdb73c49edb4b97368
niranjanadsul <niranjanadsul96@gmail.com>
2025-05-31 23:17:00 +0530

    Added complete solution for FrogJump

---
+++ src/DynamicProgramming/DP_1D/FrogJump_2.java
@@ -0,0 +1,29 @@
+package DynamicProgramming.DP_1D;
+
+public class FrogJump_2 {
+    //https://www.geeksforgeeks.org/problems/geek-jump/1
+    // to reach 0 index frog need 0 cost
+    // to reach i th index a frog can jump from i-1 or i-2 th index based on which jump is min cost
+    int minCost(int[] height) {
+        // code here
+        int[] cost = new int[height.length];
+        cost[0] = 0;
+        if(height.length>=2)
+            cost[1] = Math.abs(height[0]-height[1]);
+        int i=2;
+        while(i<height.length){
+            cost[i]=Math.min(cost[i-1]+Math.abs(height[i-1]-height[i]),
+                    cost[i-2]+Math.abs(height[i-2]-height[i]));
+            i++;
+        }
+        return cost[cost.length-1];
+    }
+
+    public static void main(String[] args) {
+        FrogJump_2 frogJump2= new FrogJump_2();
+        int cost = frogJump2.minCost(new int[]{20, 30, 40, 20});
+        System.out.println(cost);
+        cost = frogJump2.minCost(new int[]{30, 20, 50, 10, 40});
+        System.out.println(cost);
+    }
+}
