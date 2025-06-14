e69f8aba2af046b4c95246e1c3b31cd0302ba57c
niranjanadsul <niranjanadsul96@gmail.com>
2025-06-14 12:18:57 +0530

    Added solution for LongestCommonSubstring_2

---
+++ src/DynamicProgramming/DPSubsequence/DPStrings/LongestCommonSubstring_2.java
@@ -0,0 +1,39 @@
+package DynamicProgramming.DPSubsequence.DPStrings;
+
+public class LongestCommonSubstring_2 {
+    //https://www.geeksforgeeks.org/problems/longest-common-substring1452/1
+    //Here we are dealing with continuous substring
+    //check the notes
+    public int longestCommonSubstr(String s1, String s2) {
+        // code here
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        //initialize 1st row
+        for(int i =0;i<dp[0].length;i++){
+            dp[0][i]=0;
+        }
+        //initialize 1st column
+        for(int i =0;i<dp.length;i++){
+            dp[i][0]=0;
+        }
+        int len = 0;
+        for(int i = 1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    int val = dp[i-1][j-1]+1;
+                    dp[i][j]=val;
+                    len = Math.max(len,val);
+                }else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        LongestCommonSubstring_2 longestCommonSubstring2 = new LongestCommonSubstring_2();
+        System.out.println(longestCommonSubstring2.longestCommonSubstr("adac","adadac"));
+        System.out.println(longestCommonSubstring2.longestCommonSubstr("abcdgh","acdghr"));
+        System.out.println(longestCommonSubstring2.longestCommonSubstr("abc","acb"));
+    }
+}
