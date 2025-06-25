5363bcf21a71e73942464a0ec2670c3062ad6f65
niranjanadsul <niranjanadsul96@gmail.com>
2025-06-25 22:34:46 +0530

    Added InOrderTraversal

---
+++ src/Tree/BinaryTree/InOrderTraversal_1.java
@@ -0,0 +1,22 @@
+package Tree.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InOrderTraversal_1 {
+    //https://leetcode.com/problems/binary-tree-inorder-traversal/
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        inOrder(root,ls);
+        return ls;
+    }
+
+    //root,left,right
+    public void inOrder(TreeNode node,List<Integer> traversal){
+        if(node!=null){
+            inOrder(node.left,traversal);
+            traversal.add(node.val);
+            inOrder(node.right,traversal);
+        }
+    }
+}
