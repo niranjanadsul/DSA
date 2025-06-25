a9f01815a9c64cd728a3d2885f7a284eafc6e2ab
niranjanadsul <niranjanadsul96@gmail.com>
2025-06-25 22:29:51 +0530

    Added PreOrderTraversal

---
+++ src/Tree/BinaryTree/PreOrderTraversal_1.java
@@ -0,0 +1,22 @@
+package Tree.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreOrderTraversal_1 {
+    //https://leetcode.com/problems/binary-tree-preorder-traversal/
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> ls = new ArrayList<>();
+        preOrder(root,ls);
+        return ls;
+    }
+
+    //root,left,right
+    public void preOrder(TreeNode node,List<Integer> traversal){
+        if(node!=null){
+            traversal.add(node.val);
+            preOrder(node.left,traversal);
+            preOrder(node.right,traversal);
+        }
+    }
+}
