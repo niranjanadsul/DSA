4ed70399ca9563035538993166af78973e056afa
niranjanadsul <niranjanadsul96@gmail.com>
2025-06-27 22:59:08 +0530

    Added Iterative_InOrderTraversal

---
+++ src/Tree/BinaryTree/Iterative_InOrderTraversal_6.java
@@ -0,0 +1,32 @@
+package Tree.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Iterative_InOrderTraversal_6 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> in = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                TreeNode left = curr.left;
+                TreeNode rightMostOfLeftChild = getRightMostOfLeftChild(left);
+                rightMostOfLeftChild.right = curr;
+                curr.left = null;
+                curr = left;
+            }else{
+                in.add(curr.val);
+                curr = curr.right;
+            }
+        }
+        return in;
+    }
+
+    public TreeNode getRightMostOfLeftChild(TreeNode leftChild){
+        TreeNode right = leftChild;
+        while(right.right != null){
+            right = right.right;
+        }
+        return right;
+    }
+}
