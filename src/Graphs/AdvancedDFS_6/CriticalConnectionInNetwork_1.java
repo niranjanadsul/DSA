bcb08d5d36454521768ae1055c51a25277c1d086
niranjanadsul <niranjanadsul96@gmail.com>
2025-08-22 22:45:45 +0530

    Added solution for CriticalConnectionInNetwork

--- src/Graphs/AdvancedDFS_6/CriticalConnectionInNetwork_1.java
+++ src/Graphs/AdvancedDFS_6/CriticalConnectionInNetwork_1.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CriticalConnectionInNetwork_1 {
+    //https://leetcode.com/problems/critical-connections-in-a-network/
+    /*There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections
+    forming a network where connections[i] = [ai, bi] represents a connection between servers ai and bi.
+     Any server can reach other servers directly or indirectly through the network.
+    A critical connection is a connection that, if removed,
+    will make some servers unable to reach some other server.
+    Return all critical connections in the network in any order.*/
+
     //Also called as Tarjan's algorithm using timeIn and lowTime
     public static int timeStamp = 1;
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
