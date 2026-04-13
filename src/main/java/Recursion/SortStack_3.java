e598409f95b6e0c49876c129f006596678078735
niranjanadsul <niranjanadsul96@gmail.com>
2026-04-13 20:48:58 +0530

    SortStack_3

--- src/main/java/Recursion/SortStack_3.java
+++ src/main/java/Recursion/SortStack_3.java
@@ -5,6 +5,20 @@ import java.util.Stack;
 
 public class SortStack_3 {
     //https://www.geeksforgeeks.org/problems/sort-a-stack/1
+    /*
+    * Given a stack of integers st[]. Sort the stack in ascending order
+    *  (smallest element at the bottom and largest at the top).
+
+    Examples:
+
+    Input: st[] = [1, 2, 3]
+    Output: [3, 2, 1]
+    Explanation: The stack is already sorted in ascending order.
+
+    Input: st[] = [41, 3, 32, 2, 11]
+    Output: [41, 32, 11, 3, 2]
+    Explanation: After sorting, the smallest element (2) is at the bottom and the largest
+    * element (41) is at the top.*/
     //pop each element into the function stack and then position the elements
     public Stack<Integer> sort(Stack<Integer> s) {
         if(s.isEmpty())
