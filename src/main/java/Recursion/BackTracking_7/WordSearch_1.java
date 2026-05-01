7ca10d388c86a4e9651ee030b52b783ac548df9b
niranjanadsul <niranjanadsul96@gmail.com>
2026-05-01 21:00:26 +0530

    WordSearch_1

--- src/main/java/Recursion/BackTracking_7/WordSearch_1.java
+++ src/main/java/Recursion/BackTracking_7/WordSearch_1.java
@@ -4,6 +4,15 @@ import java.util.Arrays;
 
 public class WordSearch_1 {
     //https://leetcode.com/problems/word-search/description/
+    /*Given an m x n grid of characters board and a string word,
+    return true if word exists in the grid.
+    The word can be constructed from letters of sequentially adjacent cells,
+    where adjacent cells are horizontally or vertically neighboring.
+    The same letter cell may not be used more than once.
+    Example 1:
+
+    Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+    Output: true*/
     //Time complexity
     // As iterate on each cell off the grid and
     // for each cell we keep checking in 4 directions till depth = length of string (l)
