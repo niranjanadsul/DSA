f49de1db2f93eaa5030c7cbd5359bd9ec61b166e
niranjanadsul <niranjanadsul96@gmail.com>
2026-05-01 15:05:49 +0530

    WordBreak_2

--- src/main/java/Recursion/Partition_6/WordBreak_2.java
+++ src/main/java/Recursion/Partition_6/WordBreak_2.java
@@ -6,13 +6,31 @@ import java.util.List;
 
 public class WordBreak_2 {
     //https://leetcode.com/problems/word-break/description/
+    /*Given a string s and a dictionary of strings wordDict,
+    return true if s can be segmented into a space-separated sequence of one or more dictionary
+    words.
+    Note that the same word in the dictionary may be reused multiple times in the segmentation.
+    Example 1:
+
+    Input: s = "leetcode", wordDict = ["leet","code"]
+    Output: true
+    Explanation: Return true because "leetcode" can be segmented as "leet code".
+    Example 2:
+
+    Input: s = "applepenapple", wordDict = ["apple","pen"]
+    Output: true
+    Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+    Note that you are allowed to reuse a dictionary word.
+    Example 3:
+    Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    Output: false*/
     public boolean partition(String s, List<String> dict) {
         int[][] dp = new int[s.length()+1][s.length()+2];
         for(int[] arr:dp){
             Arrays.fill(arr,-1);
         }
         int valid=findPalindrome(0,1,s,dp,dict);
-        return valid==1?true:false;
+        return valid == 1;
     }
 
     //partitions start from 1 up to s.length()
