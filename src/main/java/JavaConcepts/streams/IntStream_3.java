b8913c3b4cbb1bbff61b1d8462414182ebf2cf6f
niranjanadsul <niranjanadsul96@gmail.com>
2026-08-18 22:53:16 +0530

    Int Streams

--- src/main/java/JavaConcepts/streams/IntStream_3.java
+++ src/main/java/JavaConcepts/streams/IntStream_3.java
@@ -1,15 +1,26 @@
 package JavaConcepts.streams;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class IntStream_3 {
     public static void main(String[] args) {
+        System.out.println("For loop**********");
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
-        System.out.println("**********");
+        System.out.println("exclusive**********");
         IntStream.range(0, 10).forEach(System.out::println);//range is exclusive of the last number
-        System.out.println("**********");
+        System.out.println("inclusive**********");
         IntStream.rangeClosed(0, 10).forEach(System.out::println);//rangeClosed is inclusive of the last number
+        System.out.println("**********");
+
+        List<String> ls=List.of("a","b","c","d");
+        IntStream.range(0, ls.size()).forEach(i-> System.out.println(ls.get(i)));
+        System.out.println("**********");
+        IntStream.rangeClosed(0, ls.size()-1).forEach(i-> System.out.println(ls.get(i)));
+        System.out.println("**********");
+
+        ls.forEach(System.out::println);
     }
 }
