91999b7780b692f3b09db682524a4c6d90a0f176
niranjanadsul <niranjanadsul96@gmail.com>
2026-08-21 23:15:41 +0530

    Streams count and min

--- src/main/java/JavaConcepts/streams/StreamCount_10.java
+++ src/main/java/JavaConcepts/streams/StreamCount_10.java
@@ -1,5 +1,6 @@
 package JavaConcepts.streams;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class StreamCount_10 {
@@ -14,5 +15,26 @@ public class StreamCount_10 {
         Integer[] arr = {5, 2, 8, 1, 4, 2, 5, 8};
         long count= Arrays.stream(arr).filter(number->number<5).count();
         System.out.println(count);
+
+        //calculate average
+        System.out.println("Print average of numbers**********");
+        double average= Arrays.stream(arr).mapToDouble(Integer::intValue).average().orElse(0.0);
+        System.out.println(average);
+
+        //calculate sum
+        System.out.println("Print sum of numbers**********");
+        Double sum= Arrays.stream(arr).mapToDouble(Integer::intValue).sum();
+        System.out.println(sum);
+        BigDecimal sumBigDecimal = BigDecimal.valueOf(sum);
+        System.out.println("Sum as BigDecimal: " + sumBigDecimal);
+
+        //calculate statistics
+        System.out.println("Print statistics of numbers**********");
+        var statistics= Arrays.stream(arr).mapToDouble(Integer::intValue).summaryStatistics();
+        System.out.println("Count:"+statistics.getCount());
+        System.out.println("Sum:"+statistics.getSum());
+        System.out.println("Average:"+statistics.getAverage());
+        System.out.println("Max:"+statistics.getMax());
+        System.out.println("Min:"+statistics.getMin());
     }
 }
