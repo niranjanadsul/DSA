1e1a5920df13ac3669d2e617de976cfd69015ab7
niranjanadsul <niranjanadsul96@gmail.com>
2026-08-22 13:35:19 +0530

    created a bean for streams testing

---
+++ src/main/java/JavaConcepts/streams/bean/Car.java
@@ -0,0 +1,55 @@
+package JavaConcepts.streams.bean;
+
+import java.util.List;
+
+public class Car {
+    private String name;
+    private String color;
+    private int price;
+
+    public Car(String name, String color, int price) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public static List<Car> getCars() {
+        return List.of(
+                new Car("Toyota", "Red", 20000),
+                new Car("Honda", "Blue", 18000),
+                new Car("Ford", "Black", 25000),
+                new Car("Chevrolet", "White", 22000),
+                new Car("Nissan", "Silver", 19000),
+                new Car("BMW", "Black", 35000),
+                new Car("Mercedes", "White", 40000),
+                new Car("Audi", "Red", 45000),
+                new Car("Volkswagen", "Blue", 30000),
+                new Car("Hyundai", "Silver", 17000),
+                new Car("Kia", "Red", 16000)
+        );
+    }
+}
